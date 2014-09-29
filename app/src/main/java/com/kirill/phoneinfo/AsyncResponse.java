e6be21ac9c6dc0650118fc317941e651dce75096
Kirill Zhukov <zhukov.kirill.96@gmail.com>
2014-09-28 23:56:03 -0700

    Created AsyncResponse for Async Task

---
+++ app/src/main/java/com/kirill/phoneinfo/AsyncResponse.java
@@ -0,0 +1,9 @@
+package com.kirill.phoneinfo;
+
+/**
+ * Created by kirill on 9/27/14.
+ * My GitHub: https://github.com/ZhukovKirill
+ */
+public interface AsyncResponse {
+    String processFinish(String output);
+}
